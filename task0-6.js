bbe6aaf9565bbd8a86895300b3a129a1ab65c6c2
Blessing <bntoko@gmail.com>
2021-07-10 10:43:12 +0200

    completed task 0.6

---
+++ task0-6.js
@@ -0,0 +1,13 @@
+function getMax(...nums) {
+    let maximum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > maximum) {
+            maximum = nums[i];
+        }
+    }
+
+    return maximum;
+}
+
+console.log(getMax(1, 22, 3, 25, 35, 35.3));
