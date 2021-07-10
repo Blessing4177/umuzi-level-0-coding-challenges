520b0c9134fede05cdfd42c4987337c65522ad5f
Blessing <bntoko@gmail.com>
2021-07-10 09:53:22 +0200

    completed task 0.1

---
+++ task0-1.js
@@ -0,0 +1,11 @@
+let x = 0;
+let y = 1;
+
+console.log('Value of x: ', x);
+console.log('Value of y: ', y);
+
+x += 3;
+y += x;
+
+console.log('New value of x: ', x);
+console.log('New value of y: ', y);
