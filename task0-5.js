2a400710620dbbee5c7257ba2017935d139c745d
Blessing <bntoko@gmail.com>
2021-07-10 12:23:23 +0200

    minor tweak

--- task0-5.js
+++ task0-5.js
@@ -1,9 +1,9 @@
 function calcAreaOfTriangle(x, y, z) {
     let semiPerimeter = (x + y + z) / 2;
     let heronValue = semiPerimeter * ((semiPerimeter - x) * (semiPerimeter - y) * (semiPerimeter - z));
-    let area = Math.sqrt(Math.abs(heronValue));
+    let area = Math.sqrt(Math.abs(heronValue)).toFixed(2);
 
-    return `The area of this triangle is ${area.toFixed(2)} sq.units`;
+    return `The area of this triangle is ${area} sq.units`;
 }
 
 console.log(calcAreaOfTriangle(26, 10, 3));
